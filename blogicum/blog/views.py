48e96ea628576604cdc5736a902f87113e14d1e2
Muhammad Saidov <saidov.m.05.01@gmail.com>
2024-04-01 12:01:14 +0300

    Blogicum is done

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -2,42 +2,37 @@ from datetime import datetime
 
 from django.http import Http404
 from django.db.models import Q
-from django.shortcuts import render, get_object_or_404, get_list_or_404
+from django.shortcuts import render, get_object_or_404
 
 from blog.models import Post, Category
 
 
-def index(request):
+def filter_posts(slug=None):
     posts = Post.objects.select_related(
         'category', 'author', 'location'
     ).filter(
         is_published=True,
-        category__is_published=True,
         pub_date__lt=datetime.now()
-    ).order_by('-id')[:5]
+    )
+    return (posts.filter(category__is_published=True) if slug is None
+            else posts.filter(category__slug=slug))
+
+
+def index(request):
     return render(
         request, 'blog/index.html',
-        {'post_list': posts})
+        {'post_list': filter_posts().order_by('-id')[:5]})
 
 
 def post_detail(request, post_id):
-    post = get_object_or_404(
-        Post.objects.select_related('category', 'author', 'location').filter(
-            is_published=True,
-            category__is_published=True,
-            pub_date__lt=datetime.now()
-        ), pk=post_id
-    )
+    post = get_object_or_404(filter_posts(), pk=post_id)
     return render(request, 'blog/detail.html', {'post': post})
 
 
 def category_posts(request, category_slug):
-    category = get_object_or_404(Category.objects.filter(slug=category_slug),
+    category = get_object_or_404(Category,
+                                 slug=category_slug,
                                  is_published=True)
-    posts = Post.objects.select_related('category').filter(
-        is_published=True,
-        category__slug=category_slug,
-        pub_date__lt=datetime.now()
-    ).order_by()
+    posts = filter_posts(category.slug)
     return render(request, 'blog/category.html',
                   {'category': category, 'post_list': posts})
